4e7cd845e6299076b3de658cd468d5c7eca7c100
cardealscript <cfelisberto@byupathway.edu>
2026-03-16 21:44:47 +0200

    Exercise done

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        // ==================================================
+
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world that he gave his only begotten Son");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.AllWordsHidden())
+            {
+                Console.WriteLine("All words hidden! Well done!");
+                break;
+            }
+
+            Console.Write("\nPress Enter to hide words or type 'quit' to exit: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords();
+        }
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        // split the text into words
+        string[] words = text.Split(' ');
+        foreach (string word in words)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords()
+    {
+        Random random = new Random();
+        int index = random.Next(_words.Count);
+        _words[index].Hide();
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + " ";
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+        return result;
+    }
+}
