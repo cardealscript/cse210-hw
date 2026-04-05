5d43b0ec7bccc8a9887d4eb2c7d6481e837af8a1
cardealscript <cfelisberto@byupathway.edu>
2026-04-05 03:00:38 +0200

    Week05 Homework - Inheritance

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Necessário para WritingAssignment acessar o nome
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,23 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Teste da classe base Assignment
+        Assignment a = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a.GetSummary());
+
+        Console.WriteLine("---");
+
+        // Teste da MathAssignment
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine("---");
+
+        // Teste da WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
